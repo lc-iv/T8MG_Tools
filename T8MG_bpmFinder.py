a6d1bcabcbecb729f1f35aa4a9b27b95d50411cd
lc-iv <louismconley@gmail.com>
2019-04-23 18:05:23 -0700

    Update T8MG_bpmFinder.py
    Quick update

--- T8MG_bpmFinder.py
+++ T8MG_bpmFinder.py
@@ -1,6 +1,9 @@
+# T8MG.io BPM Finder
+# iamkinglouisiv@gmail.com
+
 import librosa as lb
 
-audio_path = neekville + 'path/to/file.wav
+audio_path = 'drvive:path/to/file.wav
 
 y, sr = lb.load(audio_path, sr=44100)
 
@@ -8,7 +11,7 @@ onset_env = lb.onset.onset_strength(y, sr=sr)
 tempo = lb.beat.tempo(onset_envelope=onset_env, sr=sr)
 tempo = int(tempo)
 
-print('This track speed is ' + str(tempo) + ' bpm\'s.')
+print('The track speed is ' + str(tempo) + ' beats per minutes.')
 
 
 
