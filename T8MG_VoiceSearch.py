631a04663864da66d79b99e142087de4afc47f15
lc-iv <louismconley@gmail.com>
2019-04-25 20:20:55 -0700

    Class call change

--- T8MG_VoiceSearch.py
+++ T8MG_VoiceSearch.py
@@ -13,7 +13,7 @@ soup = BeautifulSoup(response.text, "lxml")
 # I want to give a list or pd df of first 10 page results
 # I want the text portion of the top result to be displayed
 
-for item in soup.select(".s"):
+for item in soup.select(".st"):
     print(item.text)
     engine.say(item.text, 'en')
     engine.runAndWait()
