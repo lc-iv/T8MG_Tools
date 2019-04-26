829ba97e30e0c0cd34d0552babc3f3330db299ab
lc-iv <louismconley@gmail.com>
2019-04-25 17:27:26 -0700

    Add voice response

--- T8MG_bpmFinder.py
+++ T8MG_bpmFinder.py
@@ -2,20 +2,28 @@
 # e-mail: louismconley@gmail.com
 
 import librosa as lb
+import pyttsx3
+
 
 # Create path variable pointing to where you file.wav is located
-audio_path = 'drvive:path/to/file.wav
+audio_path  = 'drvive:path/to/file.wav'
+# Create audio engine for talk back
+engine      = pyttsx3.init()
 
 # Load file.wav with at a sample rate of 44.1 khz (Album Quality)
-y, sr = lb.load(audio_path, sr=44100)
+y, sr       = lb.load(audio_path, sr=44100)
 
 # Analyze audio transients to find pace of overall audio (bpm's)
-onset_env = lb.onset.onset_strength(y, sr=sr)
-tempo = lb.beat.tempo(onset_envelope=onset_env, sr=sr)
-tempo = int(tempo)
+onset_env   = lb.onset.onset_strength(y, sr=sr)
+tempo       = lb.beat.tempo(onset_envelope=onset_env, sr=sr)
+tempo       = int(tempo)
+
+
+engine.say('The track speed is ' + str(tempo) + ' beats per minute.', 'en')
+engine.runAndWait()
 
 # Print results!
-print('The track speed is ' + str(tempo) + ' beats per minutes.')
+print('The track speed is ' + str(tempo) + ' beats per minute.')
 
 
 
