802c5e6721b27d8951224fbc52f81a4109d81e12
lc-iv <louismconley@gmail.com>
2019-04-28 09:31:33 -0700

    Create T8MG_Blockchain.py

---
+++ T8MG_Blockchain.py
@@ -0,0 +1,54 @@
+# T8MG.io Blockchain: I am using this to songwriters to safely store digital files (leak proof)
+# Louis     = dict[Python Programmer, Music Publisher, Freelancer]
+# Github    = lc-iv
+# email     = louismcoinley@gmail.com
+# wechat    = k_louis_iv
+
+
+import hashlib
+import json
+
+
+block_genesis   = {
+                   'prev_hash': None,
+                   'data_list': [4, 2, 11, 3]
+                  }
+
+
+block_2         = {
+                   'prev_hash': None,
+                   'data_list': [12, 5, 6, 24, 15, 1]
+                  }
+
+
+block_3         = {
+                   'prev_hash': None,
+                   'data_list': [7, 6, 2, 14, 8]
+                  }
+
+
+def hash_blocks(blocks):
+    prev_hash   = None
+    for block in blocks:
+        block['prev_hash']  = prev_hash
+        block_serialized    = json.dumps(block, sort_keys=True).encode('utf-8')
+        block_hash          = hashlib.sha256(block_serialized).hexdigest()
+        prev_hash           = block_hash
+    return prev_hash
+
+
+print('Original_Hash')
+print(hash_blocks([block_genesis, block_2, block_3]))
+
+
+print('Updating Blocks')
+
+dg12 = ['joe', 'jeff', 'jeremy', 'jacob']
+
+block_2['data_list'].append(dg12)
+block_3['data_list'].append(dg12[:2])
+
+print('After being tampered')
+print(hash_blocks([block_genesis, block_2, block_3]))
+
+
